8e48581fd68775f88544b21f2e84787e64799100
Ariel Jericko Gacilo <bokzgacilo@gmail.com>
2022-11-12 17:46:40 +0800

    make production environment

--- connection.php
+++ connection.php
@@ -1,15 +1,15 @@
 <?php
 
 // PRODUCTION
-// $server = "localhost";
-// $username = "u228124087_deposis";
-// $password = "!^o4mKR92qpB1Ok5@zU8";
-// $dbname = "u228124087_deposis_dev";
+$server = "localhost";
+$username = "u228124087_deposis";
+$password = "!^o4mKR92qpB1Ok5@zU8";
+$dbname = "u228124087_deposis_dev";
 
 // DEVELOPMENT
-$server = "localhost";
-$username = "root";
-$password = "";
-$dbname = "deposis_dev";
+// $server = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "deposis_dev";
 
 $conn = mysqli_connect($server, $username, $password, $dbname); 
